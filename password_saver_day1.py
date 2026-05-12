18cd6b9381477530c4d6357eda454659fb7d990b
V.Devanarayanan <vdcoder69@gmail.com>
2026-05-12 19:51:53 +0530

    Improved Password Saver- Menu option

--- password_saver_day1.py
+++ password_saver_day1.py
@@ -1,12 +1,21 @@
-site = input("Enter site: ")
-username = input("Enter username: ")
-password = input("Enter password: ")
-with open("passwords.txt", "a") as f:
-    f.write(f"{site},{username},{password}\n")
-print("Saved successfully")
-
-choice = input("Do you wish to see saved passwords(yes/no): ")
-if choice == "yes":
-    with open("passwords.txt", "r") as f:
-        print("\nSaved passwords\n")
-        print(f.read())
+while True:
+    print("Choice 1: Save a password\n")
+    print("Choice 2: View Saved passwords\n")
+    print("Choice 3: Exit\n")
+    choice = input("Enter choice")
+    if choice == "1":
+        site = input("Enter site:  ")
+        username = input("Enter username:  ")
+        password = input("Enter password:  ")
+        with open("password.txt", "a") as f:
+            f.write(f"{site},{username},{password}\n")
+        print("Saved successfully\n")
+    elif choice == "2":
+        with open("password.txt", "r")as f:
+            print("Saved passwords\n")
+            print(f.read())
+    elif choice == "3":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice")
