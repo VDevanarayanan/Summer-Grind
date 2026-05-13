0ba7ebe630e0017001e905fbb3e408400a4f422b
V.Devanarayanan <vdcoder69@gmail.com>
2026-05-13 20:38:40 +0530

    Added search

--- password_saver_day1.py
+++ password_saver_day1.py
@@ -1,8 +1,9 @@
 while True:
     print("Choice 1: Save a password\n")
     print("Choice 2: View Saved passwords\n")
-    print("Choice 3: Exit\n")
-    choice = input("Enter choice")
+    print("Choice 3: Search password\n")
+    print("Choice 4: Exit\n")
+    choice = input("Enter choice:  ")
     if choice == "1":
         site = input("Enter site:  ")
         username = input("Enter username:  ")
@@ -15,6 +16,13 @@ while True:
             print("Saved passwords\n")
             print(f.read())
     elif choice == "3":
+        site = input("Enter site: ")
+        with open("password.txt", "r")as f:
+            for line in f:
+                if site in line:
+                    print(line)
+
+    elif choice == "4":
         print("Exiting...")
         break
     else:
